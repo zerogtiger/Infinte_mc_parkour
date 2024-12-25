ef4e864905f8e2b39a59e8ff41159e30ebcbb3d2
zero_guru <38360323+zerogtiger@users.noreply.github.com>
2024-12-24 19:47:23 -0500

    Base generation done. Need to remove blocks in the way + vary difficulty

--- src/terrain/parkour/index.ts
+++ src/terrain/parkour/index.ts
@@ -32,43 +32,67 @@ export default class Parkour {
   get = (x: number, z: number) => {
     // requires further generation
     while (this.lastPosGen.x <= x) {
-      // if (this.currDirCount < 0) {
-      //   const newD = getRandom(0, 4);
-      //   switch (newD) {
-      //     case 0:
-      //       this.currDirection = direction.left_small;
-      //       break;
-      //     case 1:
-      //       this.currDirection = direction.left_large;
-      //       break;
-      //     case 2:
-      //       this.currDirection = direction.right_small;
-      //       break;
-      //     case 3:
-      //       this.currDirection = direction.right_large;
-      //       break;
-      //     case 4:
-      //       this.currDirection = direction.forward;
-      //       break;
-      //   }
-      //   this.currDirCount = getRandom(10, 20);
-      // }
+      if (this.currDirCount < 0) {
+        const newD = getRandom(0, 4);
+        switch (newD) {
+          case 0:
+            this.currDirection = direction.left_small;
+            break;
+          case 1:
+            this.currDirection = direction.left_large;
+            break;
+          case 2:
+            this.currDirection = direction.right_small;
+            break;
+          case 3:
+            this.currDirection = direction.right_large;
+            break;
+          case 4:
+            this.currDirection = direction.forward;
+            break;
+        }
+        this.currDirCount = getRandom(10, 20);
+      }
       // continue to proceed in current direction
+      let deltaX, deltaZ: number;
+      switch (this.currDirection) {
+        case direction.forward:
+          deltaX = getRandom(3, 4);
+          deltaZ = 0;
+          break;
+        case direction.left_small:
+          deltaX = 3
+          deltaZ = -1;
+          break;
+        case direction.left_large:
+          deltaX = getRandom(2, 3);
+          deltaZ = -2;
+          break;
+        case direction.right_small:
+          deltaX = 3
+          deltaZ = 1;
+          break;
+        case direction.right_large:
+          deltaX = getRandom(2, 3);
+          deltaZ = 2;
+          break;
+      }
+      this.currDirCount--;
       // if (this.currDirection == direction.forward) {
-      let gap = getRandom(3, 4);
+      // let gap = getRandom(3, 4);
 
       // if jump is impossible, fill in blocks
-      if (this.getY(this.lastPosGen.x + gap, this.lastPosGen.z) + 3 - this.lastPosGen.y >= 2) {
-        for (var i = 1; i < gap; ++i) {
-          this.mapOfCoords.set(`${this.lastPosGen.x + i},${this.lastPosGen.z}`, this.getY(this.lastPosGen.x + i, this.lastPosGen.z) + 3)
+      if (this.getY(this.lastPosGen.x + deltaX, this.lastPosGen.z + deltaZ) + 3 - this.lastPosGen.y >= 2) {
+        for (var i = 1; i < deltaX; ++i) {
+          this.mapOfCoords.set(`${this.lastPosGen.x + i},${this.lastPosGen.z + Math.floor(i/deltaX * deltaZ)}`, this.getY(this.lastPosGen.x + i, this.lastPosGen.z + Math.floor(i/deltaX * deltaZ)) + 3)
         }
       }
-      else if (gap == 4 && this.getY(this.lastPosGen.x + gap, this.lastPosGen.z) + 3 - this.lastPosGen.y >= 1) {
-        this.mapOfCoords.set(`${this.lastPosGen.x + 3},${this.lastPosGen.z}`, this.getY(this.lastPosGen.x + 3, this.lastPosGen.z) + 3)
+      else if (deltaX == 4 && this.getY(this.lastPosGen.x + deltaX, this.lastPosGen.z) + 3 - this.lastPosGen.y >= 1) {
+        this.mapOfCoords.set(`${this.lastPosGen.x + 3},${this.lastPosGen.z + deltaZ}`, this.getY(this.lastPosGen.x + 3, this.lastPosGen.z + deltaZ) + 3)
       }
       // console.log(this.lastPosGen.x + gap);
-      this.mapOfCoords.set(`${this.lastPosGen.x + gap},${this.lastPosGen.z}`, this.getY(this.lastPosGen.x + gap, this.lastPosGen.z) + 3)
-      this.lastPosGen = { x: this.lastPosGen.x + gap, y: this.getY(this.lastPosGen.x + gap, this.lastPosGen.z) + 3, z: this.lastPosGen.z }
+      this.mapOfCoords.set(`${this.lastPosGen.x + deltaX},${this.lastPosGen.z + deltaZ}`, this.getY(this.lastPosGen.x + deltaX, this.lastPosGen.z + deltaZ) + 3)
+      this.lastPosGen = { x: this.lastPosGen.x + deltaX, y: this.getY(this.lastPosGen.x + deltaX, this.lastPosGen.z + deltaZ) + 3, z: this.lastPosGen.z + deltaZ }
     }
     if (this.mapOfCoords.has(`${x},${z}`)) {
       console.log(this.mapOfCoords);
